0b2840d04bfdd1470e09c9a8720c969d521b2e63
Ghaith Ali <ghaithammar2008@gmail.com>
2016-10-10 20:32:51 +0200

    initial commit

---
+++ iSave/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iSave
+//
+//  Created by Ghaith Ali on 10/10/2016.
+//  Copyright © 2016 Ghaith Ali. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
