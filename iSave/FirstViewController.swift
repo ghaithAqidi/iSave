9918d83a8f05930cde0565d3abb7a4bbb84bc963
Ghaith Ali <ghaithammar2008@gmail.com>
2016-10-12 18:15:36 +0200

    added a working example for firebase database

--- iSave/FirstViewController.swift
+++ iSave/FirstViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 import Firebase
 
 class FirstViewController: UIViewController {
+
+    @IBOutlet weak var spentMoney: UITextField!
     
+    @IBAction func addToExpenses(_ sender: AnyObject) {
+        let rootRef = FIRDatabase.database().reference()
+        //testing that the database works:
+        let childRef = FIRDatabase.database().reference(withPath: "grocery-items")
+        let itemsRef = rootRef.child("grocery-items")
+        let milkRef = itemsRef.child("milk")
+        print(rootRef.key)   // prints: ""
+        print(childRef.key)  // prints: "grocery-items"
+        print(itemsRef.key)  // prints: "grocery-items"
+        print(milkRef.key)   // prints: "milk"
+        
+        //saving data to firebase database:
+        
+        
+    }
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let rootRef = FIRDatabase.database().reference()
-        
         
     }
 
@@ -29,8 +43,6 @@ class FirstViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-//        let testRef = rootRef.child("Testing")
-
         
     }
 
